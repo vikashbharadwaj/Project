1b63e1d7d37d21655b1b832215c2c8809c425105
Vikash  ROY <128238949+vikashbharadwaj@users.noreply.github.com>
2025-04-14 11:18:11 +0530

    Delete Udemyjpa/Udemyjpa directory

--- Udemyjpa/Udemyjpa/src/main/java/com/udemy/Udemyjpa/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.Udemyjpa.repository;
-
-import com.udemy.Udemyjpa.entiy.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
