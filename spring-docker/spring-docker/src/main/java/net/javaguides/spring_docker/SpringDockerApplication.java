a257c34320c884bc01890178edd18e659688955c
Vikash  ROY <128238949+vikashbharadwaj@users.noreply.github.com>
2025-10-02 16:58:22 +0530

    Add files via upload

---
+++ spring-docker/spring-docker/src/main/java/net/javaguides/spring_docker/SpringDockerApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+
+}
---
+++ spring-docker/spring-docker/src/test/java/net/javaguides/spring_docker/SpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.spring_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
