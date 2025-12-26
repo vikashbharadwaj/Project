7f3c4201fe61be58d27d3d7408eaf47942864fd1
Vikash  ROY <128238949+vikashbharadwaj@users.noreply.github.com>
2025-12-26 18:19:32 +0530

    Add files via upload

---
+++ springboot-kafka/based-domain/based-domain/src/test/java/com/example/baseddomain/BasedDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseddomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
